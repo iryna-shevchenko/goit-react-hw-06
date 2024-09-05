83b1e9720a1cfc9be89ac971dac8c01c346bf448
iryna-shevchenko <iryna.shevchenko.fullstack@gmail.com>
2024-09-05 16:58:26 +0300

    Work

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Import modern-normalize
 import "modern-normalize";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./components/App/App.jsx";
+import { store, persistor } from "./redux/store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
